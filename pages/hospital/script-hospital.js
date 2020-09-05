cebbfa95a538411baca2499c9080ea0c6abf904a
imamfahr <imamfahr@gmail.com>
2020-09-05 22:25:43 +0700

    search bar 80%

--- pages/hospital/script-hospital.js
+++ pages/hospital/script-hospital.js
@@ -1,24 +1,28 @@
-//autocomplete
-$(document).ready(function(){
-    $('input.autocomplete').autocomplete({
-      data: {
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'https://placehold.it/250x250'
-        "Jakarta Barat" : null,
-      },
-    });
-  });
-  
-
-
-// form select
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
   });
 
+  // Or with jQuery
 
-$(document).ready(function(){
+  $(document).ready(function(){
     $('select').formSelect();
+  });
+
+  //autocomplete
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.autocomplete');
+    var instances = M.Autocomplete.init(elems, options);
+  });
+  
+  $(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250',
+        'Jakarta Barat': null,
+      },
+    });
   });
