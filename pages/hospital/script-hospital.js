d8c6957ab1549335e031e0bc4dd42761899567bc
imamfahr <imamfahr@gmail.com>
2020-09-07 21:12:18 +0700

    list hospital done + adding database js

--- pages/hospital/script-hospital.js
+++ pages/hospital/script-hospital.js
@@ -39,3 +39,25 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   
+  // collapsible
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//tabs
+var instance = M.Tabs.init(el, options);
+
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
